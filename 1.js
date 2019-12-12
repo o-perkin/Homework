c0156a07f8d160cd8ce2577b0b5a2d918bb33878
Oleksii <perkin.oleksiy@gmail.com>
2019-12-12 14:38:26 +0200

    Lesson about DOM and chenges elements by JS

--- 1.js
+++ 1.js
@@ -1,3 +1,30 @@
+'use strict';
+
+let startbtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),       
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),      
+    levelValue = document.getElementsByClassName('level-value'),      
+    expensesValue = document.getElementsByClassName('expenses-value'),      
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),      
+    incomeValue = document.getElementsByClassName('income-value'),      
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),      
+	yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+	
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	btn0 = document.getElementsByTagName('button')[0],
+	btn1 = document.getElementsByTagName('button')[1],
+	btn2 = document.getElementsByTagName('button')[2],
+	optionalexpensesItem = document.querySelectorAll('optionalexpenses-item'),
+	chooseIncome = document.querySelector('.choose-income'),
+	savingsItem = document.querySelector('#savings'),
+	chooseSum = document.querySelector('.choose-sum'),
+	choosePercent = document.querySelector('.choose-percent'),
+	yearValue = document.querySelector('year-value'),
+	monthValue = document.querySelector('.month-value'),
+	dayValue = document.querySelector('.day-value');
+
+
+
 let  money, time;
 
 function start () {
@@ -25,7 +52,7 @@ let appData = {
 				b = prompt('Во сколько обойдется', '');
 				
 			if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
-				  && a != '' && b != '' && length < 50)  {
+				  && a != ' ' && b != '' && a.length < 50) {
 				 console.log('done');
 				appData.expenses[a] = b;
 			}	else {
@@ -41,13 +68,13 @@ let appData = {
 	}, 
     detectLevel: function () {
 		if (appData.moneyPerDay < 100) {
-			console.log('Минимальный уровень достатка');
+			console.log('Минимальный уровень достатка')
 		} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-			 console.log('Средний уровень достатка');
+			 console.log('Средний уровень достатка')
 		} else if (appData.moneyPerDay > 2000) {
-			console.log('Высокий уровень достатка');	 
+			console.log('Высокий уровень достатка')
 		} else {
-			console.log('Произошла ошибка');
+			console.log('Произошла ошибка')
 		};
 	}, 
 	checkSavings: function () {
